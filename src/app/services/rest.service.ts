5961e1dc11d6771dbdcfe1228653740a6aec1e74
Aryan-sws <mrwhite21@outlook.com>
2021-02-09 22:09:00 +0530

    new cha

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,28 +1,72 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
-  baseUrl:any
+  baseUrl:any;
   constructor(private http: HttpClient) { 
-    
   }
-  get(endPointg: string, params?: any, optn?: any): Observable<any> {
+ 
+  get(endPointg: string, params?: any, optn?: any) {
     const p = [];
-    let newParam: string;
-    // if (params) {
-    //   // tslint:disable-next-line: forin
-    //   for (const  k in params) {
-    //     const str = k + '=' + params[k];
-    //     p.push(str);
-    //   }
-    //   newParam = p.join('&');
-    // }
+    var newParam: any;
+    if (params) {
+      // tslint:disable-next-line: forin
+      for (const  k in params) {
+        const str = k + '=' + params[k];   
+        p.push(str);
+      }
+      newParam = p.join('&');
+    }
     try {
-      return this.http.get(this.baseUrl + endPointg + '?' +  optn );
+      return this.http.get(this.baseUrl + endPointg + '?' + newParam, optn );
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  post(endPointg: string, params?: any) {
+    try { return this.http.post<any>(this.baseUrl + endPointg, params); }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  delete(endPointg: string, params?: any) {
+    const p = [];
+    let newParam: any;
+    if (params) {
+      // tslint:disable-next-line: forin
+      for (const  k in params) {
+        const str = k + '=' + params[k];
+        p.push(str);
+      }
+      newParam = p.join('&');
+    }
+    try { return this.http.delete<any>(this.baseUrl + endPointg + '?' + newParam); }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  
+  put(endPointg: string, params?: any, optn?: any) {
+    const p = [];
+    let newParam: any;
+    if (params) {
+      // tslint:disable-next-line: forin
+      for (const  k in params) {
+        const str = k + '=' + params[k];
+        p.push(str);
+      }
+      newParam = p.join('&');
+    }
+    try {
+      return this.http.put<any>(this.baseUrl + endPointg + '?' + newParam, optn);
     }
     catch (e) {
       console.log(e);
