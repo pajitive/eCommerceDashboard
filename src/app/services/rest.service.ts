4dcb94401000756732f3f2ed722180bd98ff7957
Ajeet Mishra <ajeet.mishra@edinno.in>
2021-02-10 18:23:30 +0530

    bind bug fixed

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,60 +1,45 @@
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
-  baseUrl:any;
-  constructor(private http: HttpClient) { 
-  }
+  baseUrl = 'http://localhost:3030/';
+  constructor(private http: HttpClient) {  }
  
-  get(endPointg: string, params?: any, optn?: any) {
+  get(endPointg: any, params?: any, optn?: any) {
     const p = [];
-    var newParam: any;
+    let newParam: any;
     if (params) {
       // tslint:disable-next-line: forin
       for (const  k in params) {
-        const str = k + '=' + params[k];   
+        const str = k + '=' + params[k];
         p.push(str);
       }
       newParam = p.join('&');
     }
-    try {
-      return this.http.get(this.baseUrl + endPointg + '?' + newParam, optn );
-    }
-    catch (e) {
-      console.log(e);
-    }
+    return this.http.get(this.baseUrl + endPointg + '?' + newParam, optn );
   }
 
-  post(endPointg: string, params?: any) {
-    try { return this.http.post<any>(this.baseUrl + endPointg, params); }
-    catch (e) {
-      console.log(e);
-    }
+  post(endpoint: any,data: any){
+    return this.http.post(this.baseUrl + endpoint,data);
   }
 
   delete(endPointg: string, params?: any) {
     const p = [];
     let newParam: any;
     if (params) {
-      // tslint:disable-next-line: forin
       for (const  k in params) {
         const str = k + '=' + params[k];
         p.push(str);
       }
       newParam = p.join('&');
     }
-    try { return this.http.delete<any>(this.baseUrl + endPointg + '?' + newParam); }
-    catch (e) {
-      console.log(e);
-    }
+    return this.http.delete<any>(this.baseUrl + endPointg + '?' + newParam);
   }
 
-  
-  put(endPointg: string, params?: any, optn?: any) {
+  put(endPointg: string, params?: any, optn?: any){
     const p = [];
     let newParam: any;
     if (params) {
@@ -65,11 +50,6 @@ export class RestService {
       }
       newParam = p.join('&');
     }
-    try {
-      return this.http.put<any>(this.baseUrl + endPointg + '?' + newParam, optn);
-    }
-    catch (e) {
-      console.log(e);
-    }
+    return this.http.put<any>(this.baseUrl + endPointg + '?' + newParam, optn);
   }
 }
